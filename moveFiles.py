1dc6de97fba5cc9f417dc40f09e5a1e5a312e331
dylanritchings <dylanritchings1998@gmail.com>
2022-01-22 00:09:08 +0000

    semi working code, added file exists check

--- moveFiles.py
+++ moveFiles.py
@@ -3,32 +3,53 @@
 import os
 import shutil
 
-startDirectory = ""
+newDir = "/NewSamples"
+oldDir = "/OldSamples"
+startDirectory = os.getcwd()
 searchWord = ""
-new = ""
-wordDict = {'kick': './kick/', 'snare': './snare/'}
+wordDict = {"kick": "/kick/", "snare": "/snare/", "hat": "/hats/"}
+
+
+def fileExists(fPath):
+    count = 1
+    while True:
+        if os.path.exists(fPath):
+            count += 1
+        else:
+            False
+    if count == 1:
+        return fPath
+    else:
+        splitPath = fPath.split(".")
+        return splitPath[0] + count + splitPath[1]
 
 
 def moveFile(fPath, newDir):
-    shutil.move(fPath, newDir)
+    newFPath = fileExists(fPath)
+    shutil.move(newFPath, newDir)
 
-def checkFile(fPath, searchWord):
-    for word, newDir in wordDict:
-        if word in fPath:
-            moveFile(fPath, newDir)
 
-def folderLoop(fPath):
-    for filename in os.listdir(fpath):
-        folderOrFile(fPath)
+def checkFile(fPath):
+    for word, newDir in wordDict.items():
+        if word in fPath.lower():
+            moveFile(fPath, startDirectory + newDir)
+
 
 def folderOrFile(fPath):
-     if os.path.isdir(fPath):
-         folderLoop(fPath)
-     elif os.path.isfile(fPath):
-         checkFileLoop(fPath, searchWord)
+    if os.path.isdir(fPath):
+        folderLoop(fPath)
+    elif os.path.isfile(fPath):
+        checkFile(fPath)
+
+
+def folderLoop(dirPath):
+    for filename in os.listdir(dirPath):
+        folderOrFile(dirPath + "/" + filename)
+
 
 def main():
-    folderLoop(startDirectory)
+    folderLoop(startDirectory + oldDir)
+
 
 if __name__ == "__main__":
     main()
